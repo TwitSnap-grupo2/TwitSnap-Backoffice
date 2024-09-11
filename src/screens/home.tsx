2228bb9bb2cd912e7e1d5a880641cccf647dde69
Gianni Boccazzi <gianniboccazzi@gmail.com>
2024-09-11 13:08:43 -0300

    feat: landing page completed

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,12 +1,17 @@
 import styles from '../styles/home.module.css';
 import icon from '../assets/logos.png';
 const Home = () => {
-    return (
-        <div>
-            <h1 className={styles.text}>Your Gateway to Management.</h1>
-            <img src={icon} className={styles.image} />
-        </div>
-);
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <img src={icon} className={styles.image} />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.text}>Your Gateway to Management.</h1>
+        <button className={styles.button}>Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
