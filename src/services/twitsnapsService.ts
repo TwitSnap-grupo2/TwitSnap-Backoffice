09f36a0a4dc95cdef82505fb180632c4dca3b8c0
Juani <jperezdichiazza@gmail.com>
2024-10-20 18:26:33 -0300

    refactor: uncomment twitsnapsServie

--- src/services/twitsnapsService.ts
+++ src/services/twitsnapsService.ts
@@ -1,27 +1,23 @@
 import axios from "axios";
 import config from "../utils/config";
 import { getAuth } from "firebase/auth";
-import testTwits from "../data/twits";
 
 const getAllTwitSnaps = async () => {
-  // const auth = getAuth();
-  // await auth.authStateReady();
-  // const user = auth.currentUser;
-  // const token = await user?.getIdToken();
-  // console.log("🚀 ~ getAllTwitSnaps ~ token:", token);
+  const auth = getAuth();
+  await auth.authStateReady();
+  const user = auth.currentUser;
+  const token = await user?.getIdToken();
 
-  // const requestConfig = {
-  //   headers: { Authorization: `Bearer ${token}` },
-  // };
+  const requestConfig = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
 
-  // const res = await axios.get(`${config.API_GATEWAY_URL}/twits`, requestConfig);
-  // console.log("🚀 ~ getAllTwitSnaps ~ res:", res.data);
+  const res = await axios.get(`${config.API_GATEWAY_URL}/twits`, requestConfig);
 
-  // if (res.status != 200) {
-  //   throw new Error("Error while fetching twitsnaps");
-  // }
-  // return res.data;
-  return testTwits;
+  if (res.status != 200) {
+    throw new Error("Error while fetching twitsnaps");
+  }
+  return res.data;
 };
 
 export default { getAllTwitSnaps };
