82dea0df4131a0115b33d2fc30c18531909d3779
Juani <jperezdichiazza@gmail.com>
2024-11-15 11:26:47 -0300

    feat: login and password retrieving metrics

--- src/screens/Metrics.tsx
+++ src/screens/Metrics.tsx
@@ -1,6 +1,11 @@
 // import { LineChart } from "@mui/x-charts/LineChart";
 import { PieChart } from "@mui/x-charts/PieChart";
-import { LocationData, registrationData } from "../utils/data";
+import {
+  LocationData,
+  loginData,
+  passwordRecoveryData,
+  registrationData,
+} from "../utils/data";
 import { Box, CircularProgress, Typography } from "@mui/material";
 
 // totalSuccess: 150,
@@ -107,6 +112,137 @@ const Metrics = () => {
               width={400}
               height={200}
             />{" "}
+          </div>
+        </div>
+      </div>
+      <div className="rounded-xl p-10">
+        <h1 className="font-semibold text-5xl text-center mb-10">
+          Login Metrics
+        </h1>
+        <div className="flex gap-12 p-6">
+          <div className="grid grid-flow-col grid-rows-2 gap-6">
+            <div className="flex gap-5 flex-1">
+              <div className=" bg-white p-8 flex flex-col items-center gap-5 rounded-xl shadow-xl">
+                <h2 className="text-3xl font-semibold">Success Rate</h2>
+                <Box sx={{ position: "relative", display: "inline-flex" }}>
+                  <CircularProgress
+                    size={70}
+                    variant="determinate"
+                    value={Math.round(loginData.successRate * 100)}
+                  />
+                  <Box
+                    sx={{
+                      top: 0,
+                      left: 0,
+                      bottom: 0,
+                      right: 0,
+                      position: "absolute",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Typography
+                      variant="caption"
+                      component="div"
+                      fontSize={20}
+                    >{`${Math.round(
+                      loginData.successRate * 100
+                    )}%`}</Typography>
+                  </Box>
+                </Box>{" "}
+              </div>
+              <div className="bg-white flex flex-col p-8 rounded-xl items-center gap-7 flex-1 shadow-xl">
+                <h2 className="text-3xl font-semibold ">Total Success</h2>
+                <p className="text-2xl font-medium ">
+                  {loginData.totalSuccess}
+                </p>
+              </div>
+            </div>
+            <div className="bg-white flex flex-col p-8 rounded-xl items-center gap-7 shadow-xl">
+              <h2 className="text-3xl font-semibold text-center">
+                Google Logins
+              </h2>
+              <p className="text-2xl font-medium ">{loginData.googleCount}</p>
+            </div>
+            <div className="bg-white flex flex-col p-8 rounded-xl items-center gap-7 shadow-xl">
+              <h2 className="text-3xl font-semibold ">Email Logins</h2>
+              <p className="text-2xl font-medium ">{loginData.emailCount}</p>
+            </div>
+            <div className="bg-white p-8 flex flex-col items-center gap-7 rounded-xl shadow-xl">
+              <h2 className="text-3xl font-semibold text-center">
+                Average Login Time
+              </h2>
+              <p className="text-2xl font-medium text-center">
+                {loginData.averageLoginTime}
+              </p>
+            </div>
+          </div>
+          <div className="bg-white flex flex-col p-8 rounded-xl items-center gap-7 shadow-xl">
+            <h2 className="text-3xl font-semibold ">Logins by Country </h2>
+            <PieChart
+              series={[
+                {
+                  data: parseLocationData(loginData.locationCount),
+                },
+              ]}
+              width={400}
+              height={200}
+            />{" "}
+          </div>
+        </div>
+      </div>
+      <div className="rounded-xl p-10">
+        <h1 className="font-semibold text-5xl text-center mb-10">
+          Password Recovery Metrics
+        </h1>
+        <div className="flex gap-12 p-6">
+          {/* <div className="grid grid-flow-col grid-rows-2 gap-6"> */}
+          {/* <div className=""> */}
+          {/* <div className="flex gap-5 flex-1"> */}
+          <div className=" bg-white p-8 flex flex-col items-center gap-5 rounded-xl shadow-xl">
+            <h2 className="text-3xl font-semibold">Success Rate</h2>
+            <Box sx={{ position: "relative", display: "inline-flex" }}>
+              <CircularProgress
+                size={70}
+                variant="determinate"
+                value={Math.round(passwordRecoveryData.successRate * 100)}
+              />
+              <Box
+                sx={{
+                  top: 0,
+                  left: 0,
+                  bottom: 0,
+                  right: 0,
+                  position: "absolute",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Typography
+                  variant="caption"
+                  component="div"
+                  fontSize={20}
+                >{`${Math.round(
+                  passwordRecoveryData.successRate * 100
+                )}%`}</Typography>
+              </Box>
+            </Box>{" "}
+          </div>
+          <div className="bg-white flex flex-col p-8 rounded-xl items-center gap-7 flex-1 shadow-xl">
+            <h2 className="text-3xl font-semibold ">Total Success</h2>
+            <p className="text-2xl font-medium ">
+              {passwordRecoveryData.totalSuccess}
+            </p>
+          </div>
+          <div className="bg-white p-8 flex flex-col items-center gap-7 rounded-xl shadow-xl">
+            <h2 className="text-3xl font-semibold text-center">
+              Average Password Recovery Time
+            </h2>
+            <p className="text-2xl font-medium text-center">
+              {passwordRecoveryData.averageRecoverPasswordTime}
+            </p>
           </div>
         </div>
       </div>
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -24,3 +24,37 @@ export const registrationData: RegistrationData = {
   emailCount: 100,
   googleCount: 50,
 };
+
+type LoginData = {
+  totalSuccess: number;
+  successRate: number;
+  averageLoginTime: number;
+  locationCount: LocationData[];
+  emailCount: number;
+  googleCount: number;
+};
+
+export const loginData: LoginData = {
+  totalSuccess: 300,
+  successRate: 0.6434,
+  averageLoginTime: 2.1,
+  locationCount: [
+    { location: "ARG", res: 25 },
+    { location: "USA", res: 20 },
+    { location: "AUS", res: 60 },
+  ],
+  emailCount: 200,
+  googleCount: 100,
+};
+
+type PasswordRecoveryData = {
+  totalSuccess: number;
+  successRate: number;
+  averageRecoverPasswordTime: number;
+};
+
+export const passwordRecoveryData: PasswordRecoveryData = {
+  totalSuccess: 53,
+  successRate: 0.94,
+  averageRecoverPasswordTime: 1.3,
+};
